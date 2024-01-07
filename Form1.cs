3d64470e09061da0d55bf297763330e241851e2e
joaopedrodeb <joaomoraesdevdb@outlook.com>
2024-01-07 15:47:58 -0400

    #2 - Criado template win-forms para sistema pdv

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_pdv_gerenciamento_de_pedidos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
